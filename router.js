fe9b4a84e3de91536385f04ab1e1fa7644f50198
Z20070082 <Jarvan_Chen@wistron.com>
2020-10-26 11:51:08 +0800

    提交之前的错误

--- router.js
+++ router.js
@@ -76,7 +76,7 @@ router.get('/students/new', function(req, res) {
 })
 router.post('/students/new', function(req, res) {
   // console.log(req.body );
-  Student.save(req.body, function(err) {
+  new Student(req.body).save(function(err) {
     if(err) {
       return res.status(500).send('文件读取错误');
     }
@@ -86,7 +86,7 @@ router.post('/students/new', function(req, res) {
 })
 router.get('/students/edit', function(req, res) {
   // console.log(req.query.id);
-  Student.findById(parseInt(req.query.id), function(err, student) {
+  Student.findById(req.query.id, function(err, student) {
     if(err) {
       return res.status(500).send('server error')
     }
@@ -100,7 +100,7 @@ router.post('/students/edit', function(req, res) {
   // 步骤：1获取表单的数据， 2更新Student.upData数据， 3 发送响应
   // console.log(req.body);
   // console.log(req.query);
-  Student.updataById(req.body, function(err, data) {
+  Student.findByIdAndUpdate(req.body.id, req.body, function(err, data) {
     if(err) {
       return res.status(500).send('server error')
     }
@@ -111,7 +111,7 @@ router.get('/students/delete', function(req, res) {
   // 步骤： 1 获取要删除的id 2 根据id执行删除操作 3 根据操作结果响应数据
   // console.log(req.query.id);
   
-  Student.deleteById(req.query.id, function(err) {
+  Student.findByIdAndDelete(req.query.id, function(err) {
     if(err) {
       return res.status(500).send('server error');
     }
--- student.js
+++ student.js
@@ -1,105 +1,21 @@
-// 将db.js里面的数据进行进一步的封装
-
-var fs = require('fs')
-
-var dbPath = './db.json'
-
-// 封装查找学生的方法
-exports.find = function(callback) {
-  fs.readFile(dbPath, 'utf8', function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    callback(null, JSON.parse(data).students);
-  })
-}
-// 封装添加学生的方法
-exports.save = function(student, callback) {
-  fs.readFile(dbPath, 'utf8', function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    var students = JSON.parse(data).students;
-    student.id = students.length == 0 ? 1 : students[students.length - 1].id + 1
-    // student.id = students.length + 1;
-    // console.log(students[students.length - 1].id);
-    students.push(student);
-    // 把对象又转化成字符串，保存到文件
-    var fileData = JSON.stringify({
-      students: students
-    })
-    fs.writeFile(dbPath, fileData, function(error) {
-      if(error) {
-        return callback(error);
-      }
-      callback();
-    })
-  })
-}
-
-// 更新学生信息
-exports.updataById = function(student, callback) {
-  fs.readFile(dbPath, 'utf8', function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    var students = JSON.parse(data).students;
-    student.id = parseInt(student.id);
-    var stu = students.find(function(item) {
-      return item.id === student.id;
-    })
-    for(var key in student) {
-      stu[key] = student[key];
-    }
-
-    // 把对象转化成字符串
-    var fileData = JSON.stringify({
-      students: students
-    })
-    fs.writeFile(dbPath, fileData, function(error) {
-      if(error) {
-        return callback(error);
-      }
-      callback();
-    })
-  })
-}
-
-// 通过id来找到对应的数组,对应edit页面
-exports.findById = function(id, callback) {
-  fs.readFile(dbPath, 'utf8', function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    var students = JSON.parse(data).students;
-    var ret = students.find(function(item) {
-      return item.id === id;
-    })
-    callback(null, ret)
-  })
-}
-
-// 删除学生信息
-exports.deleteById = function(id, callback) {
-  fs.readFile(dbPath, 'utf8', function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    var students = JSON.parse(data).students;
-    // student.id = parseInt(student.id);
-    var deleteId = students.findIndex(function(item) {
-      return item.id === parseInt(id);
-    })
-    students.splice(deleteId, 1);
-    // 把对象转化成字符串
-  var fileData = JSON.stringify({
-    students: students
-  })
-    fs.writeFile(dbPath, fileData, function(error) {
-      if(error) {
-        return callback(error);
-      }
-      callback(null);
-    })
-  })
-}
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+mongoose.connect('mongodb://localhost/itcast')
+var studentSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: Number,
+    enum: [0, 1],
+    default: 0
+  },
+  age: {
+    type: Number
+  },
+  hobbies: {
+    type: String
+  }
+})
+module.exports = mongoose.model('Student', studentSchema)
